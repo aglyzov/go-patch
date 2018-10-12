2789dbe882e76c105b7e0255d9641ea62073c2c4
Aleksandr Glyzov <bonoba@gmail.com>
2018-10-12 10:30:07 +0300

    add more tests

--- struct_test.go
+++ struct_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Struct_Works(t *testing.T) {
+func Test_Struct_Ignores_EmptyPatch(t *testing.T) {
 
 	type Target struct {
 		FirstName string
@@ -14,21 +14,13 @@ func Test_Struct_Works(t *testing.T) {
 		Salary    int
 		Extra     string
 	}
-	type Patch struct {
-		FirstName  *string
-		LastName   *string
-		Salary     int
-		unexported bool
-		NonExisting []byte
-	}
-
 	var a = Target{
 		FirstName: "Anakin",
 		LastName:  "Skywalker",
 		Salary:    123,
 		Extra:     "unchanged",
 	}
-	var chg, err = Struct(&a, Patch{})
+	var chg, err = Struct(&a, struct{}{})
 
 	assert.NoError(t, err)
 	assert.False(t, chg)
@@ -36,52 +28,173 @@ func Test_Struct_Works(t *testing.T) {
 	assert.Equal(t, a.LastName, "Skywalker")
 	assert.Equal(t, a.Salary, 123)
 	assert.Equal(t, a.Extra, "unchanged")
+}
 
-	var p = Patch{
-		FirstName:  strPtr("Darth"),
-		LastName:   strPtr("Vader"),
-		unexported: true,
-        NonExisting: []byte{10,20},
+func Test_Struct_Ignores_UnknownFields(t *testing.T) {
+
+	type Target struct {
+		FirstName string
+		LastName  string
+		Salary    int
 	}
-	chg, err = Struct(&a, p)
+	type Patch struct {
+		MiddleName string
+		Perk       string
+	}
+
+	var a = Target{
+		FirstName: "Anakin",
+		LastName:  "Skywalker",
+		Salary:    123,
+	}
+	var p = Patch{
+		MiddleName: "Sheev",
+		Perk:       "pilot",
+	}
+
+	var chg, err = Struct(&a, p)
+
+	assert.NoError(t, err)
+	assert.False(t, chg)
+	assert.Equal(t, a.FirstName, "Anakin")
+	assert.Equal(t, a.LastName, "Skywalker")
+	assert.Equal(t, a.Salary, 123)
+}
+
+func Test_Struct_Ignores_UnexportedFields(t *testing.T) {
+
+	type Target struct {
+		Exported   string
+		unexported string
+	}
+	type Patch struct {
+		Exported   *string
+		unexported *string
+	}
+
+	var a = Target{
+		Exported:   "stormtrooper",
+		unexported: "private",
+	}
+	var p = Patch{
+		Exported:   strPtr("wookie"),
+		unexported: strPtr("leutenant"),
+	}
+
+	var chg, err = Struct(&a, p)
+
+	assert.NoError(t, err)
+	assert.True(t, chg)
+	assert.Equal(t, a.Exported, "wookie")
+	assert.Equal(t, a.unexported, "private")
+}
+
+func Test_Struct_Ignores_ZeroValueFields(t *testing.T) {
+
+	type Target struct {
+		Name   string
+		Salary int
+		OnDuty bool
+	}
+	type Patch struct {
+		Name   string
+		Salary int
+		OnDuty bool
+	}
+
+	var a = Target{
+		Name:   "Han Solo",
+		Salary: 15,
+		OnDuty: true,
+	}
+	var p = Patch{
+		Name:   "Chubacca",
+		Salary: 0,
+		OnDuty: false,
+	}
+
+	var chg, err = Struct(&a, p)
+
+	assert.NoError(t, err)
+	assert.True(t, chg)
+	assert.Equal(t, a.Name, "Chubacca")
+	assert.Equal(t, a.Salary, 15)
+	assert.Equal(t, a.OnDuty, true)
+}
+
+func Test_Struct_Handles_Pointers(t *testing.T) {
+
+	type Target struct {
+		FirstName string
+		LastName  string
+		Salary    int
+		OnDuty    bool
+	}
+	type Patch struct {
+		FirstName *string
+		LastName  *string
+		Salary    *int
+		OnDuty    *bool
+	}
+
+	var a = Target{
+		FirstName: "Anakin",
+		LastName:  "Skywalker",
+		Salary:    123,
+		OnDuty:    true,
+	}
+	var p = Patch{
+		FirstName: strPtr("Darth"),
+		LastName:  strPtr("Vader"),
+		Salary:    intPtr(0),
+		OnDuty:    boolPtr(false),
+	}
+
+	var chg, err = Struct(&a, p)
 
 	assert.NoError(t, err)
 	assert.True(t, chg)
 	assert.Equal(t, a.FirstName, "Darth")
 	assert.Equal(t, a.LastName, "Vader")
-	assert.Equal(t, a.Salary, 123)
-	assert.Equal(t, a.Extra, "unchanged")
+	assert.Equal(t, a.Salary, 0)
+	assert.Equal(t, a.OnDuty, false)
 }
 
-func Test_Struct_ThowsError(t *testing.T) {
+func Test_Struct_Detects_WrongType(t *testing.T) {
 
 	type Target struct {
 		Name   string
 		Salary int
 	}
 	type Patch struct {
-		Name   *string
-		Salary float64  // wrong type
+		Name   string
+		Salary float64 // wrong type
 	}
 
 	var a = Target{
 		Name:   "Anakin Skywalker",
 		Salary: 123,
 	}
-	var chg, err = Struct(&a, Patch{})
+	var p = Patch{
+		Name:   "Darth Vader",
+		Salary: 500.0,
+	}
 
-	assert.NoError(t, err)
-	assert.False(t, chg)
-	assert.Equal(t, a.Name, "Anakin Skywalker")
-	assert.Equal(t, a.Salary, 123)
-
-	var p = Patch{Name: strPtr("Darth Vader"), Salary: 500.0}
-	chg, err = Struct(&a, p)
+	var _, err = Struct(&a, p)
 
 	assert.Error(t, err)
 	assert.Equal(t, a.Salary, 123)
 }
 
+
+// -- test helpers --
+
 func strPtr(s string) *string {
 	return &s
+}
+func intPtr(i int) *int {
+	return &i
+}
+func boolPtr(b bool) *bool {
+	return &b
 }
